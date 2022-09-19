fcddd1b6519a3942a754fceeb534c1f4e6f11c43
Mari <mariana.cavalcantevl@gmail.com>
2022-09-19 16:24:22 -0300

    Creation of base structure of a React application; Using an API; Application of TypeScript to add static typing.

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,34 @@
+import { Repository } from "./RepositoryItem"
+import { useState, useEffect } from "react"
+import '../styles/repositories.scss'
+
+interface Repository{
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList(){
+  const [repositories, setRepositories] = useState<Repository[]>([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  },[])
+
+  console.log(repositories)
+
+  return(
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        {repositories.map(repository => {
+          return <Repository key={repository.name} repository={repository}/>
+        })}
+        
+       
+      </ul>
+    </section>
+  )
+}
